72704538a9574021f782611fe2d5146690c42c43
Lorenze Jay <Lorenzejay@gmail.com>
2020-11-29 11:08:42 -0800

    created product benefits page

--- src/components/ProductBenefits.js
+++ src/components/ProductBenefits.js
@@ -1,25 +1,77 @@
-import React from 'react';
+import React from "react"
+import BackgroundImage from "gatsby-background-image"
+import Img from "gatsby-image/withIEPolyfill"
+import { graphql, useStaticQuery } from "gatsby"
 
 const ProductBenefits = props => {
+  const data = useStaticQuery(graphql`
+    query {
+      image1: file(relativePath: { eq: "crystal1.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 500, maxHeight: 500, quality: 85) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      image2: file(relativePath: { eq: "crystal2.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 500, maxHeight: 500, quality: 85) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      image3: file(relativePath: { eq: "crystal3.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 500, maxHeight: 500, quality: 85) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      image4: file(relativePath: { eq: "crystal4.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 500, maxHeight: 500, quality: 85) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
 
+  const image1 = data.image1.childImageSharp.fluid
+  const image2 = data.image2.childImageSharp.fluid
+  const image3 = data.image3.childImageSharp.fluid
+  const image4 = data.image4.childImageSharp.fluid
+  console.log(image1)
   return (
     <section className="product-benefits">
-      <div className="product-benefits-group">
-        <div className="product-benefit-1-tagline">
-          Here is product benefit 1. Isn't it amazing?
+      <div className="product-benefits-content">
+        <div className="product-benefits-row-1">
+          <Img
+            fluid={image1}
+            style={{ height: 700, width: 550 }}
+            objectFit="cover"
+          />
+          <Img
+            fluid={image2}
+            style={{ height: 450, width: 400 }}
+            objectFit="cover"
+          />
         </div>
-        <div className="product-benefit-1-image"></div>
-        <div className="product-benefit-2-tagline">
-          Here is product benefit 2. I can do this all day long.
+        <div className="product-benefits-row-2">
+          <Img
+            fluid={image4}
+            style={{ height: 500, width: 350 }}
+            objectFit="cover"
+          />
+          <Img
+            fluid={image3}
+            style={{ height: 600, width: 450 }}
+            objectFit="cover"
+          />
         </div>
-        <div className="product-benefit-2-image"></div>
-        <div className="product-benefit-3-tagline">
-          Boom. Product benefit 3. Kills any last remaining doubts no?
-        </div>
-        <div className="product-benefit-3-image"></div>
       </div>
     </section>
   )
-};
+}
 
-export default ProductBenefits;
+export default ProductBenefits
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,24 @@
-import React from "react";
+import React from "react"
 
-import Layout from "../components/Layout";
-import SEO from "../components/SEO";
-import ValueProp from "../components/ValueProp";
-import SocialProof from "../components/SocialProof";
-import ProductBenefits from "../components/ProductBenefits";
-import ProductFeatures from "../components/ProductFeatures";
-import Testimonials from "../components/Testimonials";
-import CallToAction from "../components/CallToAction";
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+import ValueProp from "../components/ValueProp"
+import SocialProof from "../components/SocialProof"
+import ProductBenefits from "../components/ProductBenefits"
+import ProductFeatures from "../components/ProductFeatures"
+import Testimonials from "../components/Testimonials"
+import CallToAction from "../components/CallToAction"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <ValueProp />
-    <SocialProof />
+    {/* <SocialProof /> */}
     <ProductBenefits />
     <ProductFeatures />
     <Testimonials />
     <CallToAction />
   </Layout>
-);
+)
 
-export default IndexPage;
+export default IndexPage
