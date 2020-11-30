d85bf663f90434f288dfe3c5fb05bcba3a997a2f
Lorenze Jay <Lorenzejay@gmail.com>
2020-11-30 05:38:24 -0800

    modified cta

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import Button from "./Button";
+import React from "react"
+import Button from "./Button"
 
 const CallToAction = props => {
-
   return (
     <section className="cta">
-      <h1>Try our awesome product.</h1>
-      <p>There&apos;s nothing to lose. 30 days free trial.</p>
-      <Button label="Get Started" />
+      <h1>Our goal is to inspire you to believe.</h1>
+      <p>
+        We vary in sizes for those who prefer to carry crystals around with
+        them, wear them, or display them.
+      </p>
+      <p>Send us a message on Instagram.</p>
+      <a href="https://www.instagram.com/crystalsbyus/">
+        <Button label="Lets get you started" />
+      </a>
     </section>
   )
-};
+}
 
-export default CallToAction;
+export default CallToAction
