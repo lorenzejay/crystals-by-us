584a6440503833fbc2cdb061a8a2535b855a8ef9
Lorenze Jay <Lorenzejay@gmail.com>
2020-12-02 05:37:51 -0800

    brought back original backrground image

--- src/components/ValueProp.js
+++ src/components/ValueProp.js
@@ -1,11 +1,32 @@
 import React from "react"
 import Button from "./Button"
+import BackgroundImage from "gatsby-background-image"
+import { graphql, useStaticQuery } from "gatsby"
 
 const ValueProp = props => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        desktop: file(relativePath: { eq: "main.jpg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  )
+  const imageData = data.desktop.childImageSharp.fluid
   return (
     <section className="value-prop">
       <div className="value-prop-content">
-        <div className="value-main-image">
+        <BackgroundImage
+          Tag="section"
+          className="value-main-image"
+          fluid={imageData}
+          backgroundColor={`#040e18`}
+        >
           <div className="value-main-image-content">
             <h1 style={{ width: "55%" }}>Let us Help You Find Yours</h1>
             <p>-Connie and Jacob</p>
@@ -13,7 +34,17 @@ const ValueProp = props => {
               <Button label="Learn More" />
             </a>
           </div>
-        </div>
+        </BackgroundImage>
+        {/* <div className="value-main-image"> */}
+
+        {/* <div className="value-main-image-content">
+            <h1 style={{ width: "55%" }}>Let us Help You Find Yours</h1>
+            <p>-Connie and Jacob</p>
+            <a href="https://www.instagram.com/crystalsbyus/">
+              <Button label="Learn More" />
+            </a>
+          </div> */}
+        {/* </div> */}
       </div>
     </section>
   )
