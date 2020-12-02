a48e3bf7c415f084ada05c5ed78bb5c41973aad6
Lorenze Jay <Lorenzejay@gmail.com>
2020-12-02 04:51:15 -0800

    removed usused js

--- src/components/ValueProp.js
+++ src/components/ValueProp.js
@@ -1,41 +1,10 @@
 import React from "react"
 import Button from "./Button"
-import { graphql, useStaticQuery } from "gatsby"
-import BackgroundImage from "gatsby-background-image"
 
 const ValueProp = props => {
-  const data = useStaticQuery(
-    graphql`
-      query {
-        crystal: file(relativePath: { eq: "main.jpg" }) {
-          childImageSharp {
-            fluid(quality: 90, maxWidth: 1920) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-      }
-    `
-  )
-  const imageData = data.crystal.childImageSharp.fluid
-
   return (
     <section className="value-prop">
       <div className="value-prop-content">
-        {/* <BackgroundImage
-          fluid={imageData}
-          backgroundColor="#ffffff"
-          className="value-main-image"
-          style={{ backgroundFit: "cover" }}
-        >
-          <div className="value-main-image-content">
-            <h1 style={{ width: "55%" }}>Let us Help You Find Yours</h1>
-            <p>-Connie and Jacob</p>
-            <a href="https://www.instagram.com/crystalsbyus/">
-              <Button label="Learn More" />
-            </a>
-          </div>
-        </BackgroundImage> */}
         <div className="value-main-image">
           <div className="value-main-image-content">
             <h1 style={{ width: "55%" }}>Let us Help You Find Yours</h1>
