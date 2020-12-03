14d2a9e43c97fee96b27b83e565e014b14c6d0bb
Lorenze Jay <Lorenzejay@gmail.com>
2020-12-02 16:04:50 -0800

    purposeful inccorect

--- src/components/ProductBenefits.js
+++ src/components/ProductBenefits.js
@@ -32,7 +32,7 @@ const ProductBenefits = props => {
     }
   `)
 
-  const crystalInfo = data.allContentJson.nodes[1]
+  const crystalInfo = data.allContentJson.nodes[2]
   console.log("crystalifno", crystalInfo)
   return (
     <section className="product-benefits">
