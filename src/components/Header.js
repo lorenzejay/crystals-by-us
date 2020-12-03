68014a2eed5d02187076ed9060d18d2485912d5a
Lorenze Jay <Lorenzejay@gmail.com>
2020-12-03 08:21:56 -0800

    updated navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,18 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Backdrop from "./BackdropSection"
 
 const Header = ({ siteTitle }) => {
   const [isOpen, setIsOpen] = useState(false)
+  const [scroll, setScroll] = useState(0)
   const toggle = () => setIsOpen(!isOpen)
 
   let navClasses = "nav-links"
   let navItemClasses = "nav-link-item"
   let burgerClass = "burger"
   let backdropClass = "backdrop"
+
   if (isOpen) {
     navClasses = "nav-links nav-active"
     navItemClasses = "nav-link-item-active"
@@ -18,11 +20,22 @@ const Header = ({ siteTitle }) => {
     backdropClass = "backdrop backdrop-active"
   }
 
+  useEffect(() => {
+    window.addEventListener("scroll", function (event) {
+      const scroll = this.scrollY
+      setScroll(scroll)
+    })
+  }, [])
+
   return (
     <header>
       <Backdrop activeClass={backdropClass} click={toggle} />
       <nav className="navbar transparent navbar-inverse">
-        <div className="nav-belt">
+        <div
+          className={`nav-belt ${
+            scroll === 0 ? "nav-content-pop" : "nav-content-scrolled-color "
+          }`}
+        >
           <div>
             <h4>
               <Link className="logo" to="/">
