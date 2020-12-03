5c6bbebe7f00a43badf5d40a14259ab65aca8eb0
Lorenze Jay <Lorenzejay@gmail.com>
2020-12-02 17:21:17 -0800

    removed manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,19 +34,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-landing-page-starter`,
-        short_name: `landing`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#675CE2`,
-        display: `standalone`,
-        icon: `src/assets/logo.svg`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-sitemap`,
   ],
 }
